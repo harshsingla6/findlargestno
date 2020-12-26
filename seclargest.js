8a4163b22fdf568449c2cd34df86088e42f0bf4a
harsh <singlaharsh1994@gmail.com>
2020-12-26 19:39:49 +0530

    nothing

--- seclargest.js
+++ seclargest.js
@@ -1,29 +1,37 @@
-const express = require("express")
-const app = express();
+// const express = require("express")
+// const app = express();
 
-Port = 4000;
+// Port = 4000;
 
-app.listen(Port , () =>{
-    console.log("server is created")
-})
+// app.listen(Port , () =>{
+//     console.log("server is created")
+// })
 
-app.get("/",(req,res)=>{
+// app.get("/",(req,res)=>{
 
-var arr = [2,4,5,7,99,8,97,44,67,1]
-var largest = 0
-var secondlargest = 0
+var arr = [2,4,6,7,99,8,98,44,100,67,1];
+var largest = 0;
+var justlarge = 0;
+var secondlargest = 0;
   
 
 for ( i = 0; i < arr.length; i++) {
+    console.log("index",i,"value",arr[i]);
     if (largest < arr[i] ) {
+        justlarge = largest;
         largest = arr[i];
+
+        console.log(largest,"largest");
     }
-    else if(arr[i]>secondlargest && arr[i]!=largest)
-    secondlargest = arr[i];
+    if(arr[i]>justlarge && arr[i]!=largest){
+        justlarge = arr[i];
+        console.log(justlarge,"second")
+    }
 }
 
 
- console.log(secondlargest);
- res.json(secondlargest);
 
-})
+ console.log(justlarge,"second large");
+//  res.json(secondlargest);
+
+// })
